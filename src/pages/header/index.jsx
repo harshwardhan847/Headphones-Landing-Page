aa22f5d134239f213d9ec82b9ea61bdc1c054f7d
Harsh <harshwardhan847@gmail.com>
2023-02-08 07:45:16 +0530

    made build

--- src/pages/header/index.jsx
+++ src/pages/header/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "../../components/navbar";
 import "./style.css";
 import headerImg from "../../images/pngwing.png";
@@ -8,13 +8,18 @@ import {
   AiFillYoutube,
 } from "react-icons/ai";
 import { FaFacebookF } from "react-icons/fa";
+import AOS from "aos";
+import 'aos/dist/aos.css'
 
 const Header = () => {
+  useEffect(() => {
+    AOS.init({ duration: 1500 });
+  }, []);
   return (
     <div className="header">
       <div className="img-container">
-        <img src={headerImg} alt="" className="back-img"/>
-        <h4 className="social-links">
+        <img src={headerImg} alt="" className="back-img" data-aos="zoom-in"data-aos-duration="600"></img>
+        <h4 className="social-links" data-aos="fade-up">
           Follow us -
           <div className="social-icons">
             <AiOutlineTwitter />
@@ -23,17 +28,17 @@ const Header = () => {
             <AiFillYoutube />
           </div>
         </h4>
-        <div className="feature">
+        <div className="feature"data-aos="fade-up">
           <h4>Wireless</h4>
           <h4>Beats Studio 3</h4>
         </div>
       </div>
-        <div className="circle"></div>
+      <div className="circle" ></div>
       <div className="header-wrapper">
         <Navbar />
         <div className="content">
-          <h1 className="front-title">SURFACE</h1>
-          <h1 className="back-title">HEADPHONES</h1>
+          <h1 className="front-title"data-aos="slide-left">SURFACE</h1>
+          <h1 className="back-title"data-aos="slide-right">HEADPHONES</h1>
           <h3 className="price">Price: 285$</h3>
         </div>
       </div>
